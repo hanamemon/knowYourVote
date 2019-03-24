9ee371ba1a61bc5dbef96195b09d4fd4a86ca1cc
Anum Ahmad <anumqahmad@gmail.com>
2019-03-24 08:36:48 -0400

    adding all of the sensor files!

---
+++ swiping/src/people.js
@@ -0,0 +1,64 @@
+var Applicants=[
+    {
+        "id": 18963,
+
+        "firstName": "ADRIAN"
+        "lastName": "MADARO",
+        },
+        "Political Agenda": [
+       {
+        "Pro-Choice"
+        "State Funding for Charter Schools"
+        "Supports Gun Control"
+        "Supports Medicaid expansion in MA"
+        "Supports LGBTQ+ Community"
+    },
+    ]
+    {
+        "id": 18964,
+        "firstName": "ED",
+        "lastName": "MARKEY",
+        },
+
+        "Political Agenda": [
+            { "Pro-Choice"
+                "Strongly favors supporting green energy"
+                "Supports Making Voting Registration process easier"
+                "Opposes expanding military"
+                "Favors Higher Taxes on the Wealthy"
+
+
+    },
+    ]
+    {
+        "id": 18970,
+        "firstName": "ELIZABETH",
+        "lastName": "WARREN",
+       }
+        "Political Agenda": [
+            {
+                "Favors expanding ObamaCare"
+                "Opposes privatizing Social Security"
+                "Opposes Absolute Right to Gun Ownership"
+                "Favors Higher Taxes on the Wealthy"
+                "Opposes expanding the Military"
+    },
+    ]
+    {
+        "id": 18971,
+        "firstName": "SUSAN",
+        "middleName": "WILLIAMS",
+        "lastName": "GIFFORD",
+
+        "Political Agenda": [
+            {
+                "Keeping Taxes Low"
+                "Ensuring Affordable Housing"
+                "Protecting the Environment"
+                "Caring for Seniors"
+             "Supports efforts to combat opioid crisis"
+
+    },
+    ]
+}
+]
